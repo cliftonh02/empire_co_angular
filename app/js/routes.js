efcd75b25b5cb2aeb8db79b84f56566daae07078
yourUsername <your_email@example.com>
2017-01-25 10:59:57 -0500

    updated rest

--- app/js/routes.js
+++ app/js/routes.js
@@ -5,9 +5,24 @@
 
     function Router($stateProvider){
       $stateProvider
+      //Route for Main Products
       .state('products', {
         url: '/products',
         templateUrl: "../templates/pages/products/index.html",
+        controller: 'ProductsIndexController',
+        controllerAs: 'vm'
+      })
+      //Route for New Products
+      .state('newProducts', {
+        url: '/products/new',
+        templateUrl: "../templates/pages/products/new.html",
+        controller: 'ProductsIndexController',
+        controllerAs: 'vm'
+      })
+      //Route for edit Products
+      .state('editProducts', {
+        url: '/products/edit',
+        templateUrl: "../templates/pages/products/edit.html",
         controller: 'ProductsIndexController',
         controllerAs: 'vm'
       });
